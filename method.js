5eea68b5ab857e76d1aa878a1e03372a5ba7c099
Jacder <coderoj@outlook.com>
2021-03-14 15:54:33 +0800

    upd(method.js) add method REPEAT

--- method.js
+++ method.js
@@ -9,5 +9,17 @@ funcList["NULL"] = ()=>""
 funcList["OPEN"] = ut.readFile
 funcList["EVAL"] = eval
 
+funcList["REPEAT"] = function(param) {
+  const sep = "@@,", sign = "@@$"
+  let template = param.split(sep)[0]
+  let lines = param.split(sep)[1]
+  let result = []
+  for (let line of lines) {
+    line = line.trim()
+    if (line == "") { continue }
+    result.push(template.split(sign).join(line))
+  }
+  return result.join("\n")
+}
 
 exports.funcList = funcList;
