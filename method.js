149601b2851818f6b60a2b4de2a344af0f41f60b
Jacder <coderoj@outlook.com>
2021-03-23 21:16:18 +0800

    upd(method.js)
      - add Method echo
      - remove REPEAT (should be an lexer buit-in)

--- method.js
+++ method.js
@@ -6,20 +6,9 @@ let funcList = {}
 
 // built-in functions
 funcList["NULL"] = ()=>""
+funcList["ECHO"] = (s)=>s
 funcList["OPEN"] = ut.readFile
 funcList["EVAL"] = eval
 
-funcList["REPEAT"] = function(param) {
-  const sep = "@@,", sign = "@@$"
-  let template = param.split(sep)[0]
-  let lines = param.split(sep)[1].split('\n')
-  let result = []
-  for (let line of lines) {
-    line = line.trim()
-    if (line == "") { continue }
-    result.push(template.split(sign).join(line))
-  }
-  return result.join("\n")
-}
 
 exports.funcList = funcList;
