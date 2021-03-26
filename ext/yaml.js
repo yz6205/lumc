ab0300e2426a5d959b267bf7c2a07919ebbcaed3
Jacder <coderoj@outlook.com>
2021-03-26 09:00:18 +0800

    upd(ext/) move yaml part from mdutil.js to yaml.js

--- ext/yaml.js
+++ ext/yaml.js
@@ -1,20 +1,9 @@
 "use strict"
 
-const kramed = require('kramed')
 const yaml = require('yaml')
 const {} = require('../utility')
 
-function parseMd(content) {
-  content = content.split('---')
-  let result = [], enable = true
-  for (let block of content) {
-    if (enable) {
-      result.push(block)
-    }
-    enable = !enable
-  }
-  return kramed(result.join(''))
-}
+let yamlVars = new Object()
 
 function getMapItem(list, index) {
   index = index.trim().split('|')
@@ -34,18 +23,26 @@ function getMapItem(list, index) {
   return current
 }
 
-function getMdInfo(param) {
+function yamlSet(param) {
+  let [key, cont] = param.trim().splitTwo(/\s+/)
   try {
-    let [key, cont] = param.trim().splitTwo(/\s+/)
-    cont = cont.split('---')[1].split('\t').join('  ')
-    return getMapItem(yaml.parse(cont),key)
-  } catch (err) {
+    yamlVars[key] = yaml.parse(cont.split('\t').join('  '))
+  } catch(err) {
     console.error(`Warning: parsing yaml failed: ${err['message']}`)
+  }
+  return ""
+}
+
+function yamlGet(param) {
+  let [id, key] = param.trim().splitTwo(/\s+/)
+  if (!yamlVars[id]) {
+    console.error(`Warning: yaml config item '${id}' not found`)
     return ""
   }
+  return getMapItem(yamlVars[id], key)
 }
 
 let funcList = new Map()
-funcList.set('MD', parseMd)
-funcList.set('MDINFO', getMdInfo)
+funcList.set('YAMLSET', yamlSet)
+funcList.set('YAMLGET', yamlGet)
 exports.funcList = funcList
